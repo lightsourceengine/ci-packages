ca6d02fd7d80d8dce0d2d0012aa0cb20af238dc1
Daniel Anderson <804021+dananderson@users.noreply.github.com>
2020-09-17 20:41:34 -0700

    Create a raspberry pi armv7l binary containing rpi and kmsdrm video drivers.

--- scripts/build-sdl.sh
+++ scripts/build-sdl.sh
@@ -1,47 +1,29 @@
 #!/bin/sh
-
-# Builds a platform specific SDL package.
-#
-# Usage: ./build-sdl.sh [SDLVideoDriverName] [targetArch]
-#
-# Intended to be run on a target system (this is not a cross compiling script). The script creates a bundle in the
-# current working directory. The bundle contains SDL2 headers and a precompiled SDL2 shared object.
-#
-# When running on a raspberry pi (or a debian target), the following dependencies should be installed via apt:
-# - sudo apt build-dep libsdl2             # for building sdl2 and the rpi video driver
-# - sudo apt install libdrm-dev libgbm-dev # for kmsdrm video driver
-
 set -e
 
 SDL_VERSION="SDL2-2.0.12"
 SDL_SRC_ROOT="/tmp/${SDL_VERSION}"
-ARCH="$2"
+ARCH="$1"
 
 if [ ! -d "${SDL_SRC_ROOT}" ]; then
   wget -qO- https://www.libsdl.org/release/${SDL_VERSION}.tar.gz | tar xz -C /tmp
 fi
 
 if [ "${ARCH}" = "" ]; then
-  echo "Second argument must be a user defined arch (armv7l, etc)"
+  echo "Argument must be a user defined arch (armv7l, etc)"
   exit 1
 fi
 
-if [ "$1" = "rpi" ]; then
-  VIDEO_DRIVER="$1"
-elif [ "$1" = "kmsdrm" ]; then
-  VIDEO_DRIVER="$1"
-else
-  echo "First argument must be and SDL video driver (rpi or kmsdrm)"
-  exit 1
-fi
+# TODO: should x11 be included?
+VIDEO_DRIVER_FLAGS="--enable-video-rpi --enable-video-kmsdrm"
 
 cd "${SDL_SRC_ROOT}"
 rm -rf "${SDL_SRC_ROOT}/build"
-./configure --enable-video-${VIDEO_DRIVER} --disable-video-wayland --disable-video-x11 --disable-video-opengl --disable-video-vulkan
+./configure ${VIDEO_DRIVER_FLAGS} --disable-video-wayland --disable-video-x11 --disable-video-opengl --disable-video-vulkan
 make -j2
 cd -
 
-TARGET="${SDL_VERSION}-${ARCH}-${VIDEO_DRIVER}"
+TARGET="${SDL_VERSION}-${ARCH}-pi"
 TARGET_ROOT="./${TARGET}"
 
 if [ -d "${TARGET_ROOT}" ]; then
