ed9fad1f7d7a376353e991a6d6bd001bb896af65
Daniel Anderson <804021+dananderson@users.noreply.github.com>
2020-09-23 12:02:27 -0700

    Add a pi zero sdl package. Add x11 support.

--- scripts/build-sdl.sh
+++ scripts/build-sdl.sh
@@ -9,18 +9,27 @@ if [ ! -d "${SDL_SRC_ROOT}" ]; then
   wget -qO- https://www.libsdl.org/release/${SDL_VERSION}.tar.gz | tar xz -C /tmp
 fi
 
-if [ "${ARCH}" = "" ]; then
-  echo "Argument must be a user defined arch (armv7l, etc)"
-  exit 1
-fi
+case "${ARCH}" in
+  "armv6l")
+    export CFLAGS="-march=armv6zk"
+    export LDFLAGS="-march=armv6zk"
+    ;;
+  "armv7l")
+    export CFLAGS="-march=armv7-a -mfpu=neon"
+    export LDFLAGS="-march=armv7-a -mfpu=neon"
+    ;;
+  *)
+    echo "Argument must be an architecture of [armv6l, armv7l]"
+    exit 1
+    ;;
+esac
 
-# TODO: should x11 be included?
-VIDEO_DRIVER_FLAGS="--enable-video-rpi --enable-video-kmsdrm"
+VIDEO_DRIVER_FLAGS="--enable-video-rpi --enable-video-kmsdrm --enable-video-x11"
 
 cd "${SDL_SRC_ROOT}"
 rm -rf "${SDL_SRC_ROOT}/build"
-./configure ${VIDEO_DRIVER_FLAGS} --disable-video-wayland --disable-video-x11 --disable-video-opengl --disable-video-vulkan
-make -j2
+./configure ${VIDEO_DRIVER_FLAGS} --disable-video-wayland --disable-video-dummy --disable-video-opengl --disable-video-vulkan
+make ${MAKE_FLAGS:--j2}
 cd -
 
 TARGET="${SDL_VERSION}-${ARCH}-pi"
@@ -38,4 +47,3 @@ cp -r "${SDL_SRC_ROOT}/include/"* "${TARGET_ROOT}/include/SDL2"
 strip "${TARGET_ROOT}/lib/libSDL2-2.0.so.0"
 
 tar -czf "${TARGET}.tar.gz" ${TARGET}
-
